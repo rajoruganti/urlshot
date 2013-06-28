d2bf4d504939d4e9a2919eaf7d863075f37668f6
Raj Oruganti <raj@indusnet.co.in>
2013-06-28 19:33:54 +0530

    no btoa

--- routes/index.js
+++ routes/index.js
@@ -54,24 +54,16 @@ exports.shoot = function(req,res){
 	timestamp = new Date().getTime();
 	var imgName = host+"_"+timestamp+".png"
 	var imgPath = shotPath+imgName;
+	console.log("saving to:"+imgPath);
+
 	var imgUrl = "/data/"+imgName
 	webshot(req.body.url, imgPath, options, function(err) {
 	  // screenshot now saved to flickr.jpeg
 		if(err){
 			res.send("error");
 		}
-	/*	fs.readFile(imgPath, function (err, data) {
-		  		if (err) throw err;
 
-				res.writeHead(200, {
-					'Content-Type': 'image/png',
-					'Expires': 'Fri, 01 Jan 1990 00:00:00 GMT',
-		            		'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-		            		'Pragma': 'no-cache'
-				});
-		     		res.end(data);
-			});
-			*/
+		console.log("serving from:"+imgUrl);
 		res.send("<img src=\""+imgUrl+"\">");
 	});
 /*	webshot('google.com', "abcd.png",options, function(err, renderStream) {
